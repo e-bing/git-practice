c8872379a9fa82e65d100b07bfe2920287c55ee0
Yebin <ebing.code@gmail.com>
2026-01-30 11:15:56 +0900

    [feat][project]: add skeleton code for src and header

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+// #include "./header/rpi_server.h"
+// #include "./header/camera_node.h"
+// #include "./header/stm32_firmware.h"
+// #include "./header/client_ui.h"
+
+int main() {
+    printf("========== [dev] Total System Integration Test ==========\n");
+
+    // 1. 예찬: RPI 서버
+    // rpi_server_hello();
+
+    // 2. 종관: 카메라 노드
+    // camera_node_hello();
+
+    // 3. 태우: STM32 펌웨어
+    // stm32_firmware_hello();
+
+    // 4. 예빈: 클라이언트 UI
+    // client_ui_hello();
+
+    printf("=========================================================\n");
+    return 0;
+}
